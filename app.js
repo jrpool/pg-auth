85499a497a24f7c358b26064dd1dede75a535859
Jonathan Pool <pool@stulta.com>
2017-07-31 15:15:29 -0700

    Finished developing app. Not yet tested.

--- app.js
+++ app.js
@@ -3,71 +3,91 @@ const app = require('express')();
 
 // Import required modules.
 const formParser = require('body-parser').urlencoded(
-  {extended: false, inflate: false, limit: 200, parameterLimit: 2}
+  {extended: false, inflate: false, limit: 100, parameterLimit: 2}
 );
+const cookieParser = require('cookie-parser');
 
-// Define a function that returns the requested form.
-const form = method => `<!DOCTYPE html><html lang='en'>\n\n
-  <head><meta charset='utf-8'><title>Form Demo App</title></head>\n\n
-  <body>
-    <h3>Form with method set to ${method}</h3>\n\n
+// Define a function that returns an HTML document.
+const htmlDoc = bodyContent => {
+  `<!DOCTYPE html><html lang='en'>\n\n
+    <head>
+      <meta charset='utf-8'>
+      <title>Welcome back to Cookie1</title>
+    </head>\n\n
+    <body>
+      ${bodyContent}
+    </body>\n\n
+  </html>`
+};
+
+// Define a function that returns the non-personalized document.
+const anonForm = () => {
+  const bodyContent = `<h3>Welcome, stranger! What’s your name?</h3>\n\n
     <form
-      name='artist'
-      action='/submit-form'
-      method='${method.toLowerCase()}'
+      name='userInfo'
+      action='/'
+      method='post'
     >\n\n
       <p>
-        Artist Name
-        <input name='artist' type='text' size='70' maxlength='70'>
+        <label>Your name here
+          <
+            input name='userName' type='text' size='70'
+            minlength='1' maxlength='70'
+            placeholder='Your name here'
+          >
+        </label>
       </p>\n\n
-      <p>
-        Country
-        <input name='country' type='text' size='60' maxlength='60'>
-      </p>\n\n
-      <p><button type='submit'>Submit</button></p>\n\n
-    </form>\n\n
-  </body>\n\n
-</html>`;
+      <p><button type='submit'>Save my name!</button></p>\n\n
+    </form>\n\n`;
+  return htmlDoc(bodyContent);
+};
 
-// Define a function that converts an object to an indented JSON string.
-const jsonify = object => JSON.stringify(object, null, 2);
+// Define a function that returns the personalized document.
+const knownForm = userName => {
+  const bodyContent = `<h3>Welcome back, ${userName}</h3>\n\n
+    <form
+      name='userClear'
+      action='/'
+      method='post'
+    >\n\n
+      <p><button type='submit'>Clear name</button></p>\n\n
+    </form>\n\n`;
+  return htmlDoc(bodyContent);
+};
 
-/// /// CLIENT REQUEST ROUTES /// ///
+/// /// REQUEST ROUTES /// ///
 
-// Render the GET form.
+// Render the appropriate form.
 app.get(
-  '/form-get',
+  '/',
+  cookieParser,
   (req, res) => {
-    res.send(form('GET'));
-  }
-);
-
-// Render the POST form.
-app.get(
-  '/form-post',
-  (req, res) => {
-    res.send(form('POST'));
-  }
-);
-
-// Handle the form submission.
-app.all(
-  '/submit-form',
-  formParser,
-  (req, res) => {
-    const response = {'body-params': {}, 'query-params': {}};
-    if (req.method === 'GET') {
-      for (const property of Object.keys(req.query)) {
-        response['query-params'][property] = req.query[property];
-      }
+    if (req.cookies.userName) {
+      res.send(knownForm(req.cookies.userName));
     }
     else {
-      for (const property of Object.keys(req.body)) {
-        response['body-params'][property] = req.body[property];
-      }
+      res.send(anonForm);
     }
-    res.set('Content-Type', 'application/json');
-    res.send(Buffer.from(jsonify(response)));
+  }
+);
+
+// Handle a form submission.
+app.post(
+  '/submit-form',
+  cookieParser,
+  formParser,
+  (req, res) => {
+    // Handle the non-personalized (name-submission) form.
+    if (req.body.forms[0] === 'userInfo') {
+      const userName = req.body.userName;
+      res.cookie('userName', userName);
+      res.send(knownForm(userName));
+    }
+    else {
+      // Handle the personalized (cookie-clearing) form.
+      res.clearCookie(req.cookies.userName);
+      res.send(anonForm);
+    }
   }
 );
 
@@ -75,7 +95,5 @@ app.all(
 
 // Make the application listen for queries.
 app.listen(3000, () => {
-  console.log(
-    'App queriable at http://localhost:3000/form-get or http://localhost:3000/form-post'
-  );
+  console.log('App queriable at http://localhost:3000/');
 });
